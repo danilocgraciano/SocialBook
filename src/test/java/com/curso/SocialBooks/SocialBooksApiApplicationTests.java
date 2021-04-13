329acf4fc225507088c0af1a24a9c8c4741290f1
Vinicius-Faria <Viniciguto1@gmail.com>
2021-04-13 15:57:27 -0300

    Primeiro Commit

---
+++ src/test/java/com/curso/SocialBooks/SocialBooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.SocialBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
